926a8544504791df0935842ad0e83288cd4d9936
Jan Blumenkamp <jan.blumenkamp@gmx.de>
2019-01-27 16:38:09 +0000

    add getdata function

--- vectorfield.py
+++ vectorfield.py
@@ -9,12 +9,15 @@ class VectorField:
 
   def dx(self, x, y):
     return self.data[y][x][0]
-    
+
   def dy(self, x, y):
     return self.data[y][x][1]
-    
+
   def angle(self, x, y):
     return math.atan2(self.dy(x, y), self.dx(x, y)) * (90.0 / math.pi) + 90.0
-    
+
   def mag(self, x, y):
     return math.sqrt(float(self.dx(x, y)) * float(self.dx(x, y)) + float(self.dy(x, y)) * float(self.dy(x, y)))
+
+  def getData(self):
+    return self.data
